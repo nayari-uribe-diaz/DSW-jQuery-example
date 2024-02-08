b30906ccc594e6bef288984b71ecf97412174ae8
Nayari Uribe Diaz <nayariu7@gmail.com>
2024-02-08 14:06:42 -0800

    save

--- myscript.js
+++ myscript.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
+	$("#p1").click(function() {
+		$(".disappear").toggle();
+	});
+	$("#p2").dblclick(function() {
+		$("#p2").css({"color":"red"});
+	});
 	$("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
